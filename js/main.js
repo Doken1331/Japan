88734ad69a366b3789a9c5afb1fc22f7e4f8c978
Doken1331 <lass245@mail.ru>
2021-10-04 20:50:19 +0300

    changed burger

--- js/main.js
+++ js/main.js
@@ -15,10 +15,17 @@
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header__nav');
     const menuCloseItem = document.querySelector('.header__nav-close');
+    const menuLink = document.querySelector('.header__list');
     burgerItem.addEventListener('click', function() {
         menu.classList.add('header__nav_active');
+        document.body.style.overflow = 'hidden';
     })
     menuCloseItem.addEventListener('click', function() {
         menu.classList.remove('header__nav_active');
+        document.body.style.overflow = '';
+    })
+    menuLink.addEventListener('click', function() {
+        menu.classList.remove('header__nav_active');
+        document.body.style.overflow = '';
     })
 }());
